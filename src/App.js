44d9d9418476808441b5b00d99c9a0e2597ff29b
toanquoctran <toanquoctran111001@gmail.com>
2021-09-29 13:00:32 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import {Typography} from '@material-ui/core';
 
-function App() {
+import wordsToNumbers from 'words-to-numbers';
+
+import NewsCards from './components/NewsCards/NewsCards';
+import Modal from './components/Modal/Modal';
+import useStyles from './styles.js';
+
+const alanKey =
+  '64370f4c903e66c5b517887fefa45c1b2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({command, articles, number}) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber =
+            number.length > 2 ? wordsToNumbers(number, {fuzzy: true}) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
+          </div>
+        ) : null}
+        <img
+          src="https://ak.picdn.net/shutterstock/videos/3339452/thumb/10.jpg"
+          className={classes.alanLogo}
+          alt="logo"
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a
+              className={classes.link}
+              href="https://beacons.ai/tqtran11?fbclid=IwAR2kVneEeRHjk9Qe6xGGsFI3ImPLeRUkvm1m0hiXXI_UA6mlroiJlsR45tA"
+            >
+              {' '}
+              Toan Tran
+            </a>{' '}
+            -
+            <a className={classes.link} href="https://github.com/toanquoctran">
+              {' '}
+              Github
+            </a>{' '}
+            -
+            <a
+              className={classes.link}
+              href="https://www.linkedin.com/in/toanquoctran/"
+            >
+              {' '}
+              LinkedIn
+            </a>{' '}
+            <a>Powered by </a>
+            <a className={classes.link} href="https://alan.app">
+              Alan AI
+            </a>
+          </Typography>
+        </div>
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
